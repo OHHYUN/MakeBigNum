c6f702caa1f58064229c6de25b2482d5c8d4c980
OHHYUN <omplcie@gmail.com>
2021-07-18 23:43:37 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        solution("1924",2);
+    }
+
+    public static String solution(String number, int k) {
+        String answer = "";
+        return answer;
+    }
+}
