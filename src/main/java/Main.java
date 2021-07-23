401839c4e56b50476897da0422b4015a8351f273
OHHYUN <omplcie@gmail.com>
2021-07-23 23:24:31 +0900

    solve

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,27 @@ public class Main {
     }
 
     public static String solution(String number, int k) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        int cnt = number.length() - k;
+        int left = 0;
+        int right = number.length() - cnt;
+        int max = -1;
+        int idx = 0;
+
+        while(cnt > 0) {
+            max = -1;
+            for(int j = left ; j <= right ; ++j){
+                int num = number.charAt(j) - '0';
+                if(num > max){
+                    idx = j;
+                    max = num;
+                }
+            }
+            sb.append(number.charAt(idx));
+            left = idx + 1;
+            right = number.length() - --cnt;
+        }
+        System.out.println("sb.toString() = " + sb.toString());
+        return sb.toString();
     }
 }
